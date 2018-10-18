d1dc480c4fb63b9f02f15219c789006e867752cf
Jijie Chen <jijie.chen@someplace.com>
2018-10-18 21:11:52 +0800

    Added a simple website.

---
+++ Lec.Web/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Lec.Web
+{
+    class Program
+    {
+        static void Main()
+        {
+            new WebHostBuilder()
+                .Configure(app => app.Run(async context => { await context.Response.WriteAsync("Lec is started!"); }))
+                .UseKestrel()
+                .Build()
+                .Run();
+        }
+    }
+}
